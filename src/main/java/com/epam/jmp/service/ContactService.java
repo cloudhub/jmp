68d224239cd66e9862568d778f9de43e1f4950cc
Alexei Okhrimenko <storage@bitbucket.org>
2017-02-13 02:00:26 +0300

    renamed

---
+++ src/main/java/com/epam/jmp/service/ContactService.java
@@ -0,0 +1,11 @@
+package com.epam.jmp.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by alexei.okhrimenko on 07.02.2017.
+ */
+
+@Service
+public class ContactService {
+}
