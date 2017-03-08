21d5ad7b4d760f3e17654aa26fef2421a63d3220
Alexei Okhrimenko <storage@bitbucket.org>
2017-03-09 00:58:54 +0300

    refactoring

--- src/main/java/com/epam/jmp/cms/group/dao/GroupDao.java
+++ src/main/java/com/epam/jmp/cms/group/dao/GroupDao.java
@@ -4,5 +4,5 @@ package com.epam.jmp.cms.group.dao;
  * Created by alexei.okhrimenko on 09.03.2017.
  */
 public interface GroupDao {
-    public void createLabel();
+    public void createGroup();
 }
--- src/main/java/com/epam/jmp/cms/group/model/Friends.java
+++ src/main/java/com/epam/jmp/cms/group/model/Friends.java
@@ -6,7 +6,7 @@ import com.epam.jmp.cms.group.dao.GroupDao;
  * Created by alexei.okhrimenko on 08.03.2017.
  */
 public class Friends implements GroupDao {
-    public void createLabel() {
+    public void createGroup() {
         System.out.println("Friends");
     }
 }
