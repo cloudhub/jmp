cd7cd7b0f22869fbb353aa336421364c9ac3e28d
Alexei Okhrimenko <storage@bitbucket.org>
2017-03-15 03:56:54 +0300

    flyweight pattern added

---
+++ src/main/java/com/epam/jmp/patterns/flyweight/ShapeFactory.java
@@ -0,0 +1,21 @@
+package com.epam.jmp.patterns.flyweight;
+
+import java.util.HashMap;
+
+/**
+ * Created by alexei.okhrimenko on 15.03.2017.
+ */
+public class ShapeFactory {
+    private static final HashMap<String, Shape> circleMap = new HashMap();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle) circleMap.get(color);
+
+        if (circle == null) {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            System.out.println("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+}
